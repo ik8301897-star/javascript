1217c41a2ead98a76f14ee000b2bd140caf6d9c4
ishu kumar <ishukumar200706@gmail.com>
2026-05-02 21:09:41 -0700

    array

---
+++ ARRAY/app.js
@@ -0,0 +1,159 @@
+
+
+
+// let arr = [22,33,44,55];
+// console.log("array =",arr);
+//
+
+
+// let arr = new Array(11,22,33,44,55);
+// console.log(arr);
+//  console.log("type =", typeof(arr));
+
+//  let names = ["Ishu kumar", "Virat Kohli","Rohit Sharma"];
+//  console.log(names);
+
+// let data = ["Ishu Kumar", 99.80 , 24, true];
+// console.log(data[0]);
+
+// data[0]= "ishu";
+// console.log(data[0]);
+
+
+
+// let arr = [1,2,3,4,5,6,7];
+// console.log("length =", arr.length);
+
+// for(i = 0 ; i<arr.length ; i++){
+//     console.log(i,"=",arr[i]);
+    
+// }\
+
+
+
+
+// let arr = [1,2,3,4,5];
+// console.log("arr =",arr);
+// for(let i = 0 ; i<arr.length ; i++){
+//     arr[i] = arr[i] *arr[i];
+// }
+// console.log("square =",arr);
+
+
+
+
+// const readline = require("readline");
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });3
+
+// let arr = [];
+// let count = 0;
+
+// function takeInput() {
+//     if (count === 5) {
+//         console.log("Array elements are:", arr);
+//         rl.close();
+//         return;
+//     }
+
+//     rl.question(`Enter element ${count + 1}: `, (input) => {
+//         arr.push(input); // store input in array
+//         count++;
+//         takeInput(); // call again for next input
+//     });
+// }
+
+// takeInput();
+
+
+// let arr = [];
+// let n = Number(prompt("kitne element dalne hai?"));
+// for(let i = 0; i<n ; i++){
+//     arr[i] = Number(prompt("Enter value:"));
+
+// }
+// console.log(arr);
+
+// Q1  Write a program to create an array of five inputs from the user and display the elements of the array. 
+// let arr = [];
+// let n = Number(prompt("How many input ?"));
+
+// for(let i = 0 ; i<n ; i++){
+//     arr[i] = Number(prompt("Enter a number:"));
+
+// }
+// console.log(arr);
+
+
+// Q3 Write a program to create an Array and take input from the user and print their sum.
+// let arr = [2,5,9,10];
+//  let sum = 0;
+
+//  for(let i = 0; i<arr.length ; i++){
+//     sum = sum + arr[i];
+//  }
+//  console.log("sum =",sum);
+
+
+// Q4  Write a program to create and Array and take input from the user and print the average of the elements.
+
+// let arr = [6,7,3,9,10];
+// let sum = 0;
+
+// for(let i = 0 ; i<arr.length ; i++){
+//     sum = sum + arr[i];
+//     average = sum / arr.length
+    
+// }
+// console.log(average);
+
+
+// Q5 WAP to take input from user in an array and display them in reverse order?
+
+// let arr = [1,7,9,4,8];
+
+//  reverse =arr.pop();
+//  console.log(reverse);
+ 
+
+
+
+// let numbers = [1,2,3,4,5,6,7,8,9];
+//  let evencount = 0;
+//  let oddcount = 0;
+
+//  for(let i = 0; i<numbers.length ; i++){
+//     if(numbers[i] % 2 ==0){
+//         evencount++
+//     }
+//     else{
+//         oddcount++
+//     }
+
+//  }
+
+//  console.log("Evencount =", evencount);
+//  console.log("Oddcount =",oddcount);
+ 
+ 
+let n = 5;
+let arr = [2,4,6,90,1];
+ for(let i = 0; i<n ; i++){
+    console.log("Output=", arr[i]);
+    
+ }
+
+
+
+ 
+
+
+
+
+
+
+ 
+
